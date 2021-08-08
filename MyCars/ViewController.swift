9652db1d5f78c59587b945d108c9e5141ac3659b
Саша Гужавин <sashaguzavin@gmail.com>
2021-08-08 15:19:24 +0300

    Initial Commit

---
+++ MyCars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by Саша Гужавин on 08.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
