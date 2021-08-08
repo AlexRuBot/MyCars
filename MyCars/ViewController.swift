12279f4ce894fcbb6aa547adc69324b8de691c2d
Саша Гужавин <sashaguzavin@gmail.com>
2021-08-08 16:27:27 +0300

    Добавил возможность проверять есть ли данный в Coredata, и если их нет, то добавлять их из data.plist в Coredata.

--- MyCars/ViewController.swift
+++ MyCars/ViewController.swift
@@ -6,10 +6,11 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
-    lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var context: NSManagedObjectContext!
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var markLabel: UILabel!
@@ -32,11 +33,60 @@ class ViewController: UIViewController {
         
     }
     
+    private func getDataFromFile() {
+        
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
+        var records = 0
+        
+        do {
+            records = try context.count(for: fetchRequest)
+            print("Is Data there already?")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        guard records == 0 else {return}
+        
+        guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
+        let dataArray = NSArray(contentsOfFile: pathToFile)  else {return}
+        
+        for dictionary in dataArray {
+            let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
+            let car = NSManagedObject(entity: entity!, insertInto: context) as! Car
+            let carDictionary = dictionary as! [String : AnyObject]
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as! Double
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.myChoice = carDictionary["myChoice"] as! Bool
+            car.timesDriven = carDictionary["timesDriven"] as! Int16
+            
+            let imageName = carDictionary["imageData"] as? String
+            let image = UIImage(named: imageName!)
+            let imageData = image?.pngData()
+            car.imageData = imageData
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
+                car.tintColor = getColor(colorDictionary: colorDictionary)
+            }
+        }
+    }
+    
+    private func getColor(colorDictionary: [String : Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+              let green = colorDictionary["green"],
+              let blue = colorDictionary["blue"] else { return UIColor() }
+        
+        return UIColor(red: CGFloat(red / 255),
+                       green: CGFloat(green / 255),
+                       blue: CGFloat(blue / 255),
+                       alpha: 1)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        getDataFromFile()
     }
-    
-
 }
